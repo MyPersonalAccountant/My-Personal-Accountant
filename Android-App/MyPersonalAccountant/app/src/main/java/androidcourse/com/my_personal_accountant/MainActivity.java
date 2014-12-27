bd14693bca08f3d4d3ff69a4b12e8d222543213a
Emrah Bekir <emrah@emrah.it>
2014-12-27 14:48:31 +0200

    Reformate code

--- Android-App/MyPersonalAccountant/app/src/main/java/androidcourse/com/my_personal_accountant/MainActivity.java
+++ Android-App/MyPersonalAccountant/app/src/main/java/androidcourse/com/my_personal_accountant/MainActivity.java
@@ -1,7 +1,7 @@
 package androidcourse.com.my_personal_accountant;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
